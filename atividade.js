9f03bca5f4486eb086cdba2c9944c2f3ce79af0b
Tiago Henrique <tihemetene@gmail.com>
2021-02-04 22:48:00 -0300

    Add files via upload
    Inclusão da atividade 2

---
+++ atividade.js
@@ -0,0 +1,57 @@
+//1#######################################################
+const nome = "Tiago Henrique"
+//2#######################################################
+let estaChovendo = true
+//3#######################################################
+let ano = new Date().getFullYear 
+//4#######################################################
+let idade = 23 
+idade = 24 
+//5#######################################################
+let lista = [1, 2, 3, 4, 5, 6, 7, 8]
+//6#######################################################
+let aluno = {
+  nome: "Tiago",
+  ano_de_nascimento: 1997,
+  curso: "Ciência da computação"
+} 
+
+let aluno2 = {
+  nome: "João",
+  ano_de_nascimento: 2000,
+  curso: "Ciência da computação"
+}
+
+let aluno3 = {
+  nome: "Rodrigo",
+  ano_de_nascimento: 1998,
+  curso: "Administração"
+}
+//7#######################################################
+let getDados = {
+  nome,
+  estaChovendo,
+  ano,
+  idade  
+} //7
+//8#######################################################
+let x = 10
+x *= 123 
+//9#######################################################
+const listaAlunos = {aluno, aluno2, aluno3} 
+//10######################################################
+let number = 20
+number *= 42
+
+console.log(listaAlunos)
+
+
+
+
+
+
+
+
+
+
+
